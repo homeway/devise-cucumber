0b94f1cd2e2371fdf0e986827a2e2db719537889
homeway <homeway.xue@gmail.com>
2013-08-15 18:15:48 +0800

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseCucumber::Application.config.secret_token = '92defee4e30c17ca7f279d51626d2bbd85cf82e9d752c2a3b9fc7f380fa4d4254d6bcad69383bfefa4b6135955fb129857122374cb4db28aed9a82927733b64e'
